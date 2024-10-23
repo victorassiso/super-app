16264224f6689e3d7278b8cb84db3ce52539ba9c
Victor Assis <victor_assis@poli.ufrj.br>
2024-10-23 10:13:07 -0300

    feat: :tada: Implementa projeto Next.js 15 com Tailwind, Typescript, Shadcn/ui, Prettier e ESLint

---
+++ src/app/components/map/index.tsx
@@ -0,0 +1,7 @@
+export function Map() {
+  return (
+    <div className="h-full w-full bg-red-500">
+      <span>Map</span>
+    </div>
+  )
+}
---
+++ src/app/components/side-panel/index.tsx
@@ -0,0 +1,7 @@
+export function SidePanel() {
+  return (
+    <div className="w-[600px]">
+      <span>Side Panel</span>
+    </div>
+  )
+}
